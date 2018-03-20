55f0ea09c6cf07c46f3b81f8e70993f4a039fce6
shinobhi <cilindra@gmail.com>
2018-03-20 01:47:31 -0500

    Fixed cleaner.py, and made output a bit cleaner

--- BibleSimulator.py
+++ BibleSimulator.py
@@ -1,83 +1,5 @@
-#### Homework 5
-# Name: Abhiram Ilindra
-# EID: ai4948
-
-#########
-# Problem 1:
-# P(START My iguana is on fire END) = P(My | START, START)P(iguana| START, My)P(is| My, iguana)P(on| iguana, is)P(fire| is, on)P(END| on, fire)
-
-#########
-# Problem 2:
-# P(fire| is, on) = count(is, on, fire) / count(is, on, _)
-
-#########
-# Problem 3:
-# A. 2, because the probability formula takes into account neither START not END.
-# B. 1, because the probability formula takes into account both START and END.
-# C. 3, because the probability formula takes into account only START.
-
-#########
-# Problem 4:
-print("========== problem 4 ==============")
 import nltk
-
-count = { }
-
-corpus = """
-<s> I am Sam </s>
-<s> Sam I am </s>
-<s> I do not like green eggs and ham </s>
-"""
-
-words = corpus.split()
-
-for word1, word2 in nltk.bigrams(words):
-    if word1 not in count:
-        count[word1] = { }
-
-    if word2 not in count[word1]:
-        count[word1][word2] = 1
-    else:
-        count[word1][word2] += 1
-
-prob = count
-prob2 = { }
-
-for word1 in prob:
-	for word2 in prob[word1]:
-		if word1 not in prob2:
-			prob2[word1] = prob[word1][word2]
-		else:
-			prob2[word1] += prob[word1][word2]
-	for word2 in prob[word1]:
-		prob[word1][word2] = prob[word1][word2]/prob2[word1]
-
-print(prob)
-
-#########
-# Problem 5a:
-print("========== problem 5a ==============")
 import random
-current_word1 = '<s>'
-current_word2 = ''
-text = " "
-
-for i in range(1, 50):
-	k = random.random()
-	if (k < list(prob[current_word1].values())[0]):
-		current_word2 = list(prob[current_word1].keys())[0]
-	else:
-		current_word2 = list(prob[current_word1].keys())[1]
-	text = text + " " + current_word1
-	current_word1 = current_word2
-
-print(text)
-
-###########
-# Problem 5b:
-
-print("========== problem 5b ==============")
-import nltk
 
 count = { }
 
@@ -106,7 +28,7 @@ for word1 in proba:
 	for word2 in proba[word1]:
 		proba[word1][word2] = proba[word1][word2]/proba2[word1]
 
-print(proba)
+# print(proba)
 # prints my dictionary
 
 current_word1 = '<verse>'
@@ -115,7 +37,6 @@ text = " "
 
 while(True):
 	if current_word1 == '</verse>':
-		text = text + " " + current_word1
 		break;
 	k = random.random()
 	j = 0
@@ -125,7 +46,8 @@ while(True):
 			current_word2 = list(proba[current_word1].keys())[i]
 			break
 
-	text = text + " " + current_word1
+	if (current_word1 != "<verse>"):
+		text = text + " " + current_word1
 	current_word1 = current_word2
 
 print(text)
